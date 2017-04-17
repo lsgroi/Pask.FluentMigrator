b1faf3bc106696ffd1cee62b1d11d45d77c62c03
Luca Sgroi <lucasgroi@gmail.com>
2017-04-17 12:16:18 +0100

    Update dependencies

--- .build/tasks/Test-PackageInstallation.ps1
+++ .build/tasks/Test-PackageInstallation.ps1
@@ -4,10 +4,10 @@ Import-Script Pask.Tests.Infrastructure -Package Pask
 Task Test-PackageInstallation Clean, Pack-Nuspec, Push-Local, {
     $Assertion = {
         $PaskVersion = (([xml](Get-Content (Join-Path $ProjectFullPath "Pask.FluentMigrator.nuspec"))).package.metadata.dependencies.dependency | Where { $_.id -eq "Pask" }).version
-        Assert ((([xml](Get-Content (Join-Path $SolutionFullPath "Application.Domain\packages.config"))).packages.package | Where { $_.id -eq "Pask" }).version -eq $PaskVersion) "Incorrect version of Pask installed into project 'Application.Domain'"
+        Assert ((([xml](Get-Content (Join-Path $SolutionFullPath "Application\packages.config"))).packages.package | Where { $_.id -eq "Pask" }).version -eq $PaskVersion) "Incorrect version of Pask installed into project 'Application'"
         $FluentMigratorVersion = (([xml](Get-Content (Join-Path $ProjectFullPath "Pask.FluentMigrator.nuspec"))).package.metadata.dependencies.dependency | Where { $_.id -eq "FluentMigrator" }).version
-        Assert ((([xml](Get-Content (Join-Path $SolutionFullPath "Application.Domain\packages.config"))).packages.package | Where { $_.id -eq "FluentMigrator" }).version -eq $FluentMigratorVersion) "Incorrect version of FluentMigrator installed into project 'Application.Domain'"
+        Assert ((([xml](Get-Content (Join-Path $SolutionFullPath "Application\packages.config"))).packages.package | Where { $_.id -eq "FluentMigrator" }).version -eq $FluentMigratorVersion) "Incorrect version of FluentMigrator installed into project 'Application'"
     }
 
-    Test-PackageInstallation -Name Pask.FluentMigrator -Assertion $Assertion -InstallationTargetInfo "Install into 'Application.Domain' project"
+    Test-PackageInstallation -Name Pask.FluentMigrator -Assertion $Assertion -InstallationTargetInfo "Install into 'Application' project"
 }
